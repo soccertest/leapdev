7d7dd01bb0c63b602cd2b3550fd6eecc2b9c7cd1
soccertest <soccer4l20l@gmail.com>
2023-05-16 20:43:27 -0400

    Added assertions in login

--- tests/leapPage.test.js
+++ tests/leapPage.test.js
@@ -15,9 +15,43 @@ exports.leapPage = class leapPage {
 
         console.log('You visited login page');
 
-            await this.page.goto('https://demoqa.com/login');
-            await this.page.getByRole('listitem').filter({ hasText: /^Book Store$/ }).click();
-   
+        //Correct Username & Password
+        await this.page.goto('https://demoqa.com/login');
+        await this.page.getByPlaceholder('UserName').click();
+        await this.page.getByPlaceholder('UserName').fill('leapdevsydney2023');
+        await this.page.getByPlaceholder('Password').click();
+        await this.page.getByPlaceholder('Password').fill('Testing@123!');
+        await this.page.getByRole('button', { name: 'Login' }).click();
+        await expect(this.page.getByText('leapdevsydney2023'), 'leapdevsydney2023').toBeVisible();
+        await expect(this.page.getByRole('button', { name: 'Log out' }), 'Log out').toBeVisible();
+        await this.page.getByRole('button', { name: 'Log out' }).click();
+
+        //Correct Username & Incorrect Password
+        await this.page.getByPlaceholder('UserName').click();
+        await this.page.getByPlaceholder('UserName').fill('leapdevsydney2023');
+        await this.page.getByPlaceholder('UserName').press('Tab');
+        await this.page.getByPlaceholder('Password').fill('Password');
+        await this.page.getByRole('button', { name: 'Login' }).click();
+        await expect(this.page.getByText('Invalid username or password!'), 'Invalid username or password!').toBeVisible();
+
+         //Incorrect Username & Incorrect Password
+        await this.page.getByPlaceholder('UserName').click();
+        await this.page.getByPlaceholder('UserName').fill('leapdevsydney');
+        await this.page.getByText('Password :').click();
+        await this.page.getByPlaceholder('Password').click();
+        await this.page.locator('#password-wrapper div').nth(1).click();
+        await this.page.getByPlaceholder('Password').fill('Testing@123!');
+        await this.page.getByRole('button', { name: 'Login' }).click();
+        await this.page.getByText('Welcome,Login in Book StoreUserName : Password : LoginNew UserInvalid username o').click();
+        await expect(this.page.getByText('Invalid username or password!'), 'Invalid username or password!').toBeVisible();
+
+        //Incorrect Username & Incorrect Password
+        await this.page.getByPlaceholder('UserName').click();
+        await this.page.getByPlaceholder('UserName').fill('test');
+        await this.page.getByPlaceholder('UserName').press('Tab');
+        await this.page.getByPlaceholder('Password').fill('test');
+        await this.page.getByRole('button', { name: 'Login' }).click();
+        await expect(this.page.getByText('Invalid username or password!'), 'Invalid username or password!').toBeVisible();
 
 
     }
--- tests/leapdevtask2.test.js
+++ tests/leapdevtask2.test.js
@@ -1,8 +1,11 @@
-
-
-
 import { test, expect } from '@playwright/test';
 
+/*import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+    reporter: 'line',
+});*/
+//Importing classes
 const { leapPage } = require('./leapPage.test');
 const { leapForms } = require('./leapForms.test');
 const { leapBooks } = require('./leapBooks.test');
